0815ebbcd99e5b0c8143e60adc6118a7a7934297
darksr789 <dyt27380@gmail.com>
2026-04-29 02:28:59 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -8,32 +8,74 @@ let router = express.Router();
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
+    
     async function SURYA_X_PAIR() {
         const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
         try {
             let bot = DARK_SURYA({
-                auth: { creds: state.creds, keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' })) },
+                auth: { 
+                    creds: state.creds, 
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' })) 
+                },
                 printQRInTerminal: false,
                 logger: pino({ level: 'fatal' }),
                 browser: Browsers.macOS('Chrome')
             });
+
             if (!bot.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
                 const code = await bot.requestPairingCode(num);
-                if (!res.headersSent) { await res.send({ code }); }
+                if (!res.headersSent) { 
+                    await res.send({ code }); 
+                }
             }
+
             bot.ev.on('creds.update', saveCreds);
             bot.ev.on('connection.update', async (s) => {
-                if (s.connection === 'open') {
+                const { connection } = s;
+                if (connection === 'open') {
                     await delay(5000);
-                    await bot.sendMessage(bot.user.id, { text: "SURYA-X~" + id });
+                    let session_id = "SURYA-X~" + id;
+                    
+                    let message = `
+╔════════════════════◇
+║『 SESSION CONNECTED』
+║ ✨ SURYA-X 🔷
+║ ✨ SURYAX OFFICIAL🔷
+╚════════════════════╝
+
+---
+
+╔════════════════════◇
+║『 YOU'VE CHOSEN SURYA-X 』
+║ -Set the session ID in Heroku:
+║ - SESSION_ID: ${session_id}
+╚════════════════════╝
+╔════════════════════◇
+║ 『••• _V𝗶𝘀𝗶𝘁 𝗙𝗼𝗿_H𝗲𝗹𝗽 •••』
+║❍ 𝐎𝐰𝐧𝐞𝐫: +917797099719
+║❍ 𝐑𝐞𝐩𝐨: https://github.com/darksurya345/SURYA-X 
+║❍ 𝐖𝐚𝐆𝗿𝐨𝐮𝐩: https://chat.whatsapp.com/?mode=wwt
+║❍ 𝐖𝐚𝐂𝐡𝐚𝗻𝗻𝗲𝗹: https://whatsapp.com/channel/0029Vb64JNKJf05UHKREBM1h
+║
+║ ☬ ☬ ☬ ☬
+╚═════════════════════╝
+𒂀 Enjoy SURYA-X
+
+---
+Don't Forget To Give Star⭐ To My Repo`;
+
+                    await bot.sendMessage(bot.user.id, { text: message });
                     await delay(2000);
                     await fs.remove('./temp/' + id);
                 }
             });
-        } catch (err) { if(!res.headersSent) res.send({ code: "Error" }); }
+        } catch (err) { 
+            if(!res.headersSent) res.send({ code: "Error" }); 
+        }
     }
     SURYA_X_PAIR();
 });
+
 module.exports = router;
