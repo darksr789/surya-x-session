6755232dd35296d1453744de542d02712f52a591
darksr789 <dyt27380@gmail.com>
2026-05-01 01:33:41 +0530

    Delete id.js

--- id.js
+++
@@ -1,9 +0,0 @@
-function makeid(num = 10) {
-  let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-}
-module.exports = { makeid };
